790ec7a3f758d640d45090305ef77f35e5637cca
ybdesire <ybdesire@gmail.com>
2020-09-09 17:38:44 +0800

    add patterns with and/or logic.

---
+++ behaviors.py
@@ -0,0 +1,8 @@
+behavior_patterns = {
+    'get_phone_num':[
+        {
+            'permission_list':['android.permission.READ_PHONE_STATE'],
+            'api_list':['Landroid/telephony/TelephonyManager;->getLine1Number()Ljava/lang/String;']
+        }
+    ]
+}
